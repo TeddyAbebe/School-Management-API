25c3281d17933cc8e8bf1077e1fd3fae555823f4
Tewodros Abebe <teddyab729@gmail.com>
2024-01-31 17:27:38 +0300

    Connect to Database

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const http = require("http");
+
+require("./config/dbConnect");
+
 const app = require("./app/app");
 
 const PORT = process.env.PORT || 2016;

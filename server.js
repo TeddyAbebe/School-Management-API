6b92940e996759c62e3c7ea96c3dbfdcb90885ee
Tewodros Abebe <teddyab729@gmail.com>
2024-01-31 16:39:08 +0300

    .

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const app = require("./app/app");
-
-const PORT = process.env.PORT || 2016;
-
-// Server
-const server = http.createServer(app);
-server.listen(PORT, console.log(`Server is running on port ${PORT} `));

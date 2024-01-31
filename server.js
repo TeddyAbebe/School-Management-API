3fd5ad55181ca0906531f68b703889a3a0b2a161
Tewodros Abebe <teddyab729@gmail.com>
2024-01-31 17:34:41 +0300

    .

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 const http = require("http");
-
 require("./config/dbConnect");
 
 const app = require("./app/app");
-
 const PORT = process.env.PORT || 2016;
 
 // Server
